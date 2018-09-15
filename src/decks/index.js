50877006c75ccf273221579b22872517e2331e67
Kyle Lin <kylejlin.us@gmail.com>
2018-09-14 21:48:38 -0700

    Redesign decks to be environment agnostic (only show dev decks in dev and not in production)

--- src/decks/index.js
+++ src/decks/index.js
@@ -1,7 +1,18 @@
-// import sample from './sample';
+import sample from './sample';
 import lesson1_1phrases from './lesson1_1phrases';
 
-export default [
-  // sample,
+const productionDecks = [
   lesson1_1phrases,
 ];
+
+const devDecks = [
+  sample,
+  ...productionDecks
+];
+
+const isProduction = !(
+  !process.env.NODE_ENV
+  || process.env.NODE_ENV === 'development'
+);
+
+export default isProduction ? productionDecks : devDecks;
