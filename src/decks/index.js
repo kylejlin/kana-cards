32303a9c9fba4f052f7495a286db31496517439a
Kyle Lin <kylejlin.us@gmail.com>
2018-09-15 16:53:32 -0700

    Add Lesson 1.2 Phrases

--- src/decks/index.js
+++ src/decks/index.js
@@ -1,8 +1,10 @@
 import sample from './sample';
 import lesson1_1phrases from './lesson1_1phrases';
+import lesson1_2phrases from './lesson1_2phrases';
 
 const productionDecks = [
   lesson1_1phrases,
+  lesson1_2phrases,
 ];
 
 const devDecks = [
