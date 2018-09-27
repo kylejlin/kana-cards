07a098b39eaa661e9e13da1fd69b6dfce544a87a
Kyle Lin <kylejlin.us@gmail.com>
2018-09-26 21:04:12 -0700

    Add 1.2 Essentials

--- src/decks/index.js
+++ src/decks/index.js
@@ -1,11 +1,13 @@
 import sample from './sample';
 import lesson1_1phrases from './lesson1_1phrases';
 import lesson1_2phrases from './lesson1_2phrases';
+import lesson1_2essentials from './lesson1_2essentials';
 import lesson1_3essentials from './lesson1_3essentials';
 
 const productionDecks = [
   lesson1_1phrases,
   lesson1_2phrases,
+  lesson1_2essentials,
   lesson1_3essentials,
 ];
 
