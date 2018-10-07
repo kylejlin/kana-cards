4e2151bfc7881fb4931cbda7ca83a554470339b3
Kyle Lin <kylejlin.us@gmail.com>
2018-10-07 16:01:57 -0700

    Add 1.4 Phrases

--- src/decks/index.js
+++ src/decks/index.js
@@ -3,12 +3,14 @@ import lesson1_1phrases from './lesson1_1phrases';
 import lesson1_2phrases from './lesson1_2phrases';
 import lesson1_2essentials from './lesson1_2essentials';
 import lesson1_3essentials from './lesson1_3essentials';
+import lesson1_4phrases from './lesson1_4phrases';
 
 const productionDecks = [
   lesson1_1phrases,
   lesson1_2phrases,
   lesson1_2essentials,
   lesson1_3essentials,
+  lesson1_4phrases,
 ];
 
 const devDecks = [
