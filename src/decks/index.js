05adee8959f8dcceedadec441b918c9678743f34
Kyle Lin <kylejlin.us@gmail.com>
2018-10-07 20:17:41 -0700

    Add 1.3 Phrases

--- src/decks/index.js
+++ src/decks/index.js
@@ -2,6 +2,7 @@ import sample from './sample';
 import lesson1_1phrases from './lesson1_1phrases';
 import lesson1_2phrases from './lesson1_2phrases';
 import lesson1_2essentials from './lesson1_2essentials';
+import lesson1_3phrases from './lesson1_3phrases';
 import lesson1_3essentials from './lesson1_3essentials';
 import lesson1_4phrases from './lesson1_4phrases';
 import lesson1_4essentials from './lesson1_4essentials';
@@ -10,6 +11,7 @@ const productionDecks = [
   lesson1_1phrases,
   lesson1_2phrases,
   lesson1_2essentials,
+  lesson1_3phrases,
   lesson1_3essentials,
   lesson1_4phrases,
   lesson1_4essentials,
