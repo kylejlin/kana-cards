1e0904e6afeef773e40069e2981ad7d570bba85f
Kyle Lin <kylejlin.us@gmail.com>
2018-11-30 18:51:31 -0800

    Make canvas touchmove listener { passive: false }
    In order to prevent scrolling on Safari, touch event listeners
    must be registered with addEventListener(..., { passive: false }).
    Since React uses synthetic events (registered passively), it is
    necessary to use addEventListener() in componentDidMount()
    instead of onTouchMove.

--- src/containers/WritingDrill.js
+++ src/containers/WritingDrill.js
@@ -4,87 +4,105 @@ import HomeButton from "../components/HomeButton";
 import Header from "../components/Header";
 import AffirmationSwipeIndicator from "../components/AffirmationSwipeIndicator";
 
-export default ({
-  deckName,
-  remainingCards,
-  isTopCardRevealed,
-  selectedSwipeDirection,
-  normalizedDelta,
-  areWritingCorrectionsEnabled,
+export default class extends React.Component {
+  render() {
+    const {
+      deckName,
+      remainingCards,
+      isTopCardRevealed,
+      selectedSwipeDirection,
+      normalizedDelta,
+      areWritingCorrectionsEnabled,
 
-  onHome,
-  onPenStart,
-  onPenMove,
-  onPenEnd,
-  onReveal,
-  onAffirmationSwipeStart,
-  onAffirmationSwipeMove,
-  onAffirmationSwipeEnd,
+      onHome,
+      onPenStart,
+      onPenMove,
+      onPenEnd,
+      onReveal,
+      onAffirmationSwipeStart,
+      onAffirmationSwipeMove,
+      onAffirmationSwipeEnd,
 
-  canvasRef,
-}) => {
-  if (isTopCardRevealed) {
+      canvasRef,
+    } = this.props;
+
+    if (isTopCardRevealed) {
+      return (
+        <>
+          <Header background="blue">{deckName}</Header>
+          <HomeButton color="blue" onClick={onHome} />
+          <canvas
+            onTouchStart={
+              areWritingCorrectionsEnabled
+                ? onPenStart
+                : onAffirmationSwipeStart
+            }
+            onTouchMove={
+              areWritingCorrectionsEnabled ? onPenMove : onAffirmationSwipeMove
+            }
+            onTouchEnd={
+              areWritingCorrectionsEnabled ? onPenEnd : onAffirmationSwipeEnd
+            }
+            onMouseDown={
+              areWritingCorrectionsEnabled
+                ? onPenStart
+                : onAffirmationSwipeStart
+            }
+            onMouseMove={
+              areWritingCorrectionsEnabled ? onPenMove : onAffirmationSwipeMove
+            }
+            onMouseUp={
+              areWritingCorrectionsEnabled ? onPenEnd : onAffirmationSwipeEnd
+            }
+            width={window.innerWidth}
+            height={window.innerHeight * 0.62}
+            ref={canvasRef}
+          />
+          <div
+            className="WritingDrill__CharacterContainer"
+            onTouchStart={onAffirmationSwipeStart}
+            onTouchMove={onAffirmationSwipeMove}
+            onTouchEnd={onAffirmationSwipeEnd}
+          >
+            <div className="WritingDrill__Characters">
+              {remainingCards[0].characters}
+            </div>
+          </div>
+          <AffirmationSwipeIndicator
+            selectedSwipeDirection={selectedSwipeDirection}
+            normalizedDelta={normalizedDelta}
+          />
+        </>
+      );
+    }
     return (
       <>
         <Header background="blue">{deckName}</Header>
         <HomeButton color="blue" onClick={onHome} />
         <canvas
-          onTouchStart={
-            areWritingCorrectionsEnabled ? onPenStart : onAffirmationSwipeStart
-          }
-          onTouchMove={
-            areWritingCorrectionsEnabled ? onPenMove : onAffirmationSwipeMove
-          }
-          onTouchEnd={
-            areWritingCorrectionsEnabled ? onPenEnd : onAffirmationSwipeEnd
-          }
-          onMouseDown={
-            areWritingCorrectionsEnabled ? onPenStart : onAffirmationSwipeStart
-          }
-          onMouseMove={
-            areWritingCorrectionsEnabled ? onPenMove : onAffirmationSwipeMove
-          }
-          onMouseUp={
-            areWritingCorrectionsEnabled ? onPenEnd : onAffirmationSwipeEnd
-          }
+          onTouchStart={onPenStart}
+          onTouchMove={onPenMove}
+          onMouseDown={onPenStart}
+          onMouseMove={onPenMove}
+          onMouseUp={onPenEnd}
           width={window.innerWidth}
           height={window.innerHeight * 0.62}
           ref={canvasRef}
         />
-        <div
-          className="WritingDrill__CharacterContainer"
-          onTouchStart={onAffirmationSwipeStart}
-          onTouchMove={onAffirmationSwipeMove}
-          onTouchEnd={onAffirmationSwipeEnd}
-        >
-          <div className="WritingDrill__Characters">
-            {remainingCards[0].characters}
-          </div>
+        <div className="WritingDrill__Pinyin" onClick={onReveal}>
+          {remainingCards[0].pinyin}
         </div>
-        <AffirmationSwipeIndicator
-          selectedSwipeDirection={selectedSwipeDirection}
-          normalizedDelta={normalizedDelta}
-        />
       </>
     );
   }
-  return (
-    <>
-      <Header background="blue">{deckName}</Header>
-      <HomeButton color="blue" onClick={onHome} />
-      <canvas
-        onTouchStart={onPenStart}
-        onTouchMove={onPenMove}
-        onMouseDown={onPenStart}
-        onMouseMove={onPenMove}
-        onMouseUp={onPenEnd}
-        width={window.innerWidth}
-        height={window.innerHeight * 0.62}
-        ref={canvasRef}
-      />
-      <div className="WritingDrill__Pinyin" onClick={onReveal}>
-        {remainingCards[0].pinyin}
-      </div>
-    </>
-  );
-};
+
+  componentDidMount() {
+    this.props.canvasRef.current.addEventListener(
+      "touchmove",
+      e => {
+        e.preventDefault();
+      },
+      { passive: false },
+    );
+  }
+}
